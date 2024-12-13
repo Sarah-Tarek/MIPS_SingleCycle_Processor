8ab27679c3062211fb25634b68b3cfcba55a8995
sarah-tarek <sarahtarekbishry@gmail.com>
2024-12-13 16:48:26 +0200

    added Register_File module

--- src/MIPS.v
+++ src/MIPS.v
@@ -4,7 +4,7 @@ module MUX2to1(in0, in1, sel, out);
     input sel;
     output [31:0] out;
 
-    assign out = (sel == 0) ? in0 : in1;
+    assign out = sel ? in1 : in0;
 endmodule
 
 
@@ -25,4 +25,32 @@ module ALU(in1, in2, alu_op, zero, out);
     end
 endmodule
 
-// 
+
+// Register File
+/*
+clk (input): clock
+reg_write (input): register write enable
+read_reg1 (input): Specifies the address of the first source register to be read.
+read_reg2 (input): Specifies the address of the second source register to be read.
+write_reg (input): Specifies the address of the destination register to be written.
+write_data (input): the data to write to the destination register write_reg when reg_write is enabled.
+read_data1 & read_data2 (outputs): These are the values read from the registers specified by read_reg1 and read_reg2.
+*/
+module Register_File(clk, reg_write, read_reg1, read_reg2, write_reg, write_data, read_data1, read_data2);
+  input clk, reg_write;
+  input [4:0] read_reg1, read_reg2, write_reg;
+  input [31:0] write_data;
+  output [31:0] read_data1, read_data2;
+
+  reg [31:0] registers [0:31];  // array of 32 registers each of 32 bits
+
+  assign read_data1 = registers[read_reg1];
+  assign read_data2 = registers[read_reg2];
+
+  always @(posedge clk) begin
+    if (reg_write) registers[write_reg] <= write_data;
+    end
+endmodule
+
+
+
