255b1addd02846791499662e90e40663ece49416
sarah-tarek <sarahtarekbishry@gmail.com>
2024-12-20 22:41:33 +0200

    added 2 tb and initialized the data memory with 0

--- rtl/datapath/data_memory.v
+++ rtl/datapath/data_memory.v
@@ -8,10 +8,21 @@ module data_memory(
     // Each word in memory is 32 bits: 16,384 / 4 = 4096 words
     reg [31:0] memory [0:4096]; // 4096 words each 32-bit
 
+    // Memory Initialization (Default value 0)
+    integer i;
+    initial begin
+        // Initialize all memory locations to zero
+        for (i = 0; i < 4096; i = i + 1) begin
+            memory[i] = 32'h00000000;  // Initialize each word to zero
+        end
+    end
+
+    // Write process
     always @(posedge clk) begin
         if (mem_write)
             memory[address[13:2]] <= write_data;
     end
-
+    
+    // Read process
     assign read_data = mem_read ? memory[address[13:2]] : 32'b0;
 endmodule
